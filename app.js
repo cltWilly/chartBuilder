be1ed980b3e7b0907c4e0d2a97a6bb66ec5ac39a
cltWilly <vojtakrucky@gmail.com>
2023-08-05 19:00:15 +0200

    added vertical lines at the start of each bar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-//load data from JSON file
+// Load data from JSON file
 const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 function generateSvgElements(gpus) {
@@ -8,28 +8,34 @@ function generateSvgElements(gpus) {
 
     // Calculate the maximum FPS value for scaling
     const maxFps = Math.max(...gpuKeys.map(key => Math.max(gpus[key].fps, gpus[key].fps2)));
-  
+
     const barWidthFactor = 400 / maxFps;
-  
+
     // Generate SVG elements for GPUs
     const svgElements = gpuKeys.map((key, index) => {
-      const gpu = gpus[key];
-      const bar1Width = gpu.fps * barWidthFactor;
-      const bar2Width = gpu.fps2 * barWidthFactor;
-      return `
-        <text x="20" y="${80 + index * 100}" font-family="Arial" font-size="16">${gpu.name}</text>
-        <rect x="300" y="${60 + index * 100}" width="${bar1Width}" height="40" fill="blue" />
-        <text x="${310 + bar1Width}" y="${85 + index * 100}" font-family="Arial" font-size="12">${gpu.fps} FPS</text>
-        <rect x="300" y="${100 + index * 100}" width="${bar2Width}" height="40" fill="green" />
-        <text x="${310 + bar2Width}" y="${125 + index * 100}" font-family="Arial" font-size="12">${gpu.fps2} FPS</text>
-      `;
+        const gpu = gpus[key];
+        const bar1Width = gpu.fps * barWidthFactor;
+        const bar2Width = gpu.fps2 * barWidthFactor;
+        const verticalLineX = 300; // X-coordinate for the vertical line
+
+        return `
+            <text x="20" y="${80 + index * 100}" font-family="Arial" font-size="16">${gpu.name}</text>
+            <!-- Vertical line -->
+            <line x1="${verticalLineX}" y1="${50 + index * 100}" x2="${verticalLineX}" y2="${160 + index * 100}" stroke="black" stroke-width="1" />
+            <!-- Bar 1 -->
+            <rect x="${verticalLineX}" y="${60 + index * 100}" width="${bar1Width}" height="40" fill="blue" />
+            <text x="${verticalLineX + 10}" y="${85 + index * 100}" font-family="Arial" font-size="12">${gpu.fps} FPS</text>
+            <!-- Bar 2 -->
+            <rect x="${verticalLineX}" y="${100 + index * 100}" width="${bar2Width}" height="40" fill="green" />
+            <text x="${verticalLineX + 10}" y="${125 + index * 100}" font-family="Arial" font-size="12">${gpu.fps2} FPS</text>
+        `;
     }).join('');
-  
+
     return svgElements;
 }
 
 function generateSvgTemplate(cpu) {
-    //rename cpu from parameter to chart
+    // Rename cpu from parameter to chart
     const chart = cpu;
     return `
       <svg width="800" height="${70 + (Object.keys(chart.gpus).length * 100)}" xmlns="http://www.w3.org/2000/svg">
@@ -38,11 +44,11 @@ function generateSvgTemplate(cpu) {
         ${generateSvgElements(chart.gpus)}
       </svg>
     `;
-  }
-  
-  data.forEach((cpu, index) => {
+}
+
+data.forEach((cpu, index) => {
     const svgTemplate = generateSvgTemplate(cpu);
     const outputFile = `chart_cpu${index + 1}.svg`;
     fs.writeFileSync(outputFile, svgTemplate);
     console.log(`Chart saved to ${outputFile}`);
-  });
+});
